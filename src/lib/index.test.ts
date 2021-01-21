99949e021299965eb3d589e022b94f51f2315cb6
kamataryo <kamataryo@users.noreply.github.com>
2021-01-21 15:17:00 +0900

    Some fix

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -4,7 +4,7 @@ test('Should hash tile index as xxxx-xxxx-xxxx-xxxx', () => {
     const indexX = 1234567
     const indexY = 54321
     const digest = hashXY(indexX, indexY)
-    expect(digest).toHaveLength(16 + 3) // 16 digits + 3 hyphEns
+    expect(digest).toHaveLength(16 + 3) // 16 digits + 3 hyphens
     expect(digest.split('-').every(section => section.length === 4)).toBe(true)
     expect(digest).toMatchSnapshot()
 })
