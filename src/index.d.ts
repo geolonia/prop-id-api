204152280fccf90ebd7d5f82fe3453e715e5d026
kamataryo <kamataryo@users.noreply.github.com>
2021-01-21 18:44:33 +0900

    w.i.p.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
     interface ProcessEnv {
+        readonly ZOOM: string
         readonly INCREMENTP_VERIFICATION_API_ENDPOINT: string
         readonly INCREMENTP_VERIFICATION_API_KEY: string
     }
--- src/public.ts
+++ src/public.ts
@@ -1,12 +1,68 @@
 import Lambda from 'aws-lambda'
+import { verifyAddress, coord2XY, hashXY } from './lib/index'
 
-export const handler: Lambda.Handler = async (event, context, callback) => {
+type Handler = (event: Lambda.APIGatewayProxyEvent, context: any, callback: Lambda.Callback) => void
+
+export const handler: Handler = async (event, context, callback) => {
+
+    const address = event.queryStringParameters?.q
+    const ZOOM = parseInt(process.env.ZOOM, 10)
+
+    if(Number.isNaN(ZOOM)) {
+        return callback(JSON.stringify({
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: 'Invalid Server Error.'
+            })
+        }))
+    }
+
+    if(!address) {
+        return callback(JSON.stringify({
+            statusCode: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: 'Missing querystring parameter `q`.'
+            })
+        }))
+    }
+
+    let result
+    try {
+        result = await verifyAddress(address)
+    } catch (error) {
+        // TODO: error handling for Increment P API
+        return callback(JSON.stringify({
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: 'Internal Server Error.'
+            })
+        }))
+    }
+
+    const [lng, lat] = result.features[0] as [number, number]
+    const { x, y } = coord2XY([lat, lng], ZOOM)
+    const hash = hashXY(x, y)
+
+    const ID = `${prefCode}_${hash}`
+
     return callback(null, {
+        statusCode: 200,
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            'hello': 'world'
-        }),
+        body: JSON.stringify([
+            {
+                ID: ID
+            }
+        ]),
     });
 }
