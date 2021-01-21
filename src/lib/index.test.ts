dff05482c87413f8436f8d5047c841a7a9f54f8e
kamataryo <kamataryo@users.noreply.github.com>
2021-01-21 16:58:41 +0900

    Add coord -> tileXY calculator

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,4 +1,4 @@
-import { hashXY } from './index'
+import { hashXY, coord2XY } from './index'
 
 test('Should hash tile index as xxxx-xxxx-xxxx-xxxx', () => {
     const indexX = 1234567
@@ -7,4 +7,13 @@ test('Should hash tile index as xxxx-xxxx-xxxx-xxxx', () => {
     expect(digest).toHaveLength(16 + 3) // 16 digits + 3 hyphens
     expect(digest.split('-').every(section => section.length === 4)).toBe(true)
     expect(digest).toMatchSnapshot()
+})
+
+test('Should calculate tile indexes from coordinates', () => {
+    // see https://maps.gsi.go.jp/development/tileCoordCheck.html#18/35.68122/139.76755
+    const lat = 35.68122
+    const lng = 139.76755
+    const {x, y} = coord2XY([lat, lng], 18)
+    expect(x).toEqual(232847)
+    expect(y).toEqual(103226)
 })
