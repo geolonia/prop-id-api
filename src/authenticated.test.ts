0f19f9f93621220b1a40685990e67ef4592996c2
kamataryo <kamataryo@users.noreply.github.com>
2021-01-22 16:43:31 +0900

    Add DynamoDB close #12 close #13

--- src/authenticated.test.ts
+++
@@ -1,6 +0,0 @@
-import { promisify } from './__tests__/utils'
-import { handler } from './authenticated'
-
-test.only('wip', async () => {
-    const result = await promisify(handler)({ queryStringParameters: { apiKey: 'geolonia' } }, {})
-})
--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
     interface ProcessEnv {
         readonly ZOOM: string
         readonly AWS_DYNAMODB_API_KEY_TABLE_NAME: string
+        readonly AWS_DYNAMODB_ESTATE_ID_TABLE_NAME: string
         readonly INCREMENTP_VERIFICATION_API_ENDPOINT: string
         readonly INCREMENTP_VERIFICATION_API_KEY: string
     }
