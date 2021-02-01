d58c95f8a2f01ad4fd7ea8f04c0f4e9f90c59e6e
kamataryo <kamataryo@users.noreply.github.com>
2021-02-01 17:23:42 +0900

    WIP Add debugger and its special endpoint to attach CORS header

---
+++ src/debug.ts
@@ -0,0 +1,15 @@
+import { handler as publicHandler } from './public'
+
+export const handler: EstateAPI.LambdaHandler = async (event, context, callback) => {
+    return await publicHandler(event, context, (_0, arguedProxyResult) => {
+        const proxyResult = {
+            ...arguedProxyResult,
+            headers: {
+                ...arguedProxyResult?.headers,
+                'Access-Control-Allow-Origin': 'https://geolonia.github.io'
+            }
+        }
+        // @ts-ignore
+        return callback(_0, proxyResult)
+    })
+}
