6bd817b0d10b97ebf55b1fd822c63550f66e1004
Keitaroh Kobayashi <keita@kbys.me>
2021-04-15 09:55:18 +0900

    Allow demo from api.propid.jp

--- src/demo.ts
+++ src/demo.ts
@@ -10,6 +10,7 @@ const refererHeads = [
   'http://127.0.0.1:',
   'http://localhost:',
   'https://geolonia.github.io',
+  'https://api.propid.jp',
 ]
 
 const _handler: Handler<PublicHandlerEvent, APIGatewayProxyResult> = async (event, context, callback) => {
