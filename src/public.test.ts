cdb5152cdc84d2509982cd98184f413ac307cbd7
kamataryo <kamataryo@users.noreply.github.com>
2022-07-19 10:48:10 +0900

    Add a test for banchi go separation

--- src/public.test.ts
+++ src/public.test.ts
@@ -814,3 +814,35 @@ test('小字と建物名の分離が正しくなされる', async () => {
     "other": "おはようビル"
   })
 })
+
+test.only('addressPending であっても、建物名と緯度経度が分離できる', async () => {
+  const addr1 = '東京都世田谷区新町二丁目18-8おはようビル 201号室'
+  const addr2 = '東京都世田谷区新町二丁目18-8おはようビル'
+  const addr3 = '東京都世田谷区新町二丁目18-8'
+  const { apiKey, accessToken } = await dynamodb.createApiKey(`tries to create estate ID for ${addr1}`);
+
+  const createEvent =  (addr: string) => ({
+    queryStringParameters: { q: addr, 'api-key': apiKey },
+    headers: { 'X-Access-Token': accessToken },
+  })
+
+  const events = [addr1, addr2, addr3].map(addr => createEvent(addr))
+
+  const bodies: any[] = []
+
+  for (const event of events) {
+    // @ts-ignore
+    const lambdaResult = await handler(event) as APIGatewayProxyResult
+    const body = JSON.parse(lambdaResult.body)
+    bodies.push(body[0])
+  }
+
+  const IDs = bodies.map(body => body.ID)
+  const statuses = bodies.map(body => body.status)
+  const addrObjects = bodies.map(body => body.address.ja)
+  const banchiGos = addrObjects.map(addrObj => addrObj.address2)
+
+  expect(statuses.every(status => status === 'addressPending')).toBe(true)
+  expect(banchiGos.every(banchiGo => banchiGo === '18-8')).toBe(true)
+  expect(IDs.every(id => id === IDs[0])).toBe(true)
+})
