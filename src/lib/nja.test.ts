1a914a1de58b79d4a8abb1d5c2fb3dda8cb7e530
kamataryo <6292312+kamataryo@users.noreply.github.com>
2022-09-12 15:43:40 +0900

    WIP

--- src/lib/nja.test.ts
+++ src/lib/nja.test.ts
@@ -3,19 +3,16 @@ import { normalize } from './nja';
 
 it('works', async () => {
   const resp = await normalize('東京都文京区千石4丁目15-7');
-  expect(resp).toMatchObject({
-    "pref": "東京都",
-    "city": "文京区",
-    "town": "千石四丁目",
-    "addr": "15-7",
-    "lat": 35.729052,
-    "lng": 139.740683,
-    "level": 3,
+  expect(resp).toContain({
+    pref: "東京都",
+    city: "文京区",
+    town: "千石四丁目",
+    gaiku: 15,
+    jyukyo: 7,
+    level: 8,
   });
 });
 
 it('uses a versioned API for japanese-addresses', async () => {
-  expect(NJAConfig.japaneseAddressesApi).toStrictEqual(
-    'https://japanese-addresses.geolonia.com/v0.2.0/ja',
-  );
+  expect(NJAConfig.japaneseAddressesApi).toMatchSnapshot();
 });
